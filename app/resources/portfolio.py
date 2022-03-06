d172047e7ddd243671e23206b3ce382d58b9f3ba
Douglas Starnes <douglas.a.starnes@gmail.com>
2022-03-06 11:33:16 -0600

    load user on jwt

--- app/resources/portfolio.py
+++ app/resources/portfolio.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, current_user
 
 from app.models.profile import Profile
 from app.models.portfolio import Portfolio
@@ -11,9 +11,7 @@ from app import db
 class PortfolioResource(Resource):
     @jwt_required()
     def get(self):
-        current_user = get_jwt_identity()
-        profile = Profile.query.filter_by(username=current_user).first()
-        portfolio = Portfolio.query.filter_by(profile=profile).all()
+        portfolio = Portfolio.query.filter_by(profile=current_user).all()
         portfolio_dict = {}
         for investment in portfolio:
             coin = Coin.query.filter_by(coin_id=investment.coin_id).order_by(Coin.timestamp.desc()).first()
@@ -25,19 +23,14 @@ class PortfolioResource(Resource):
 class ManagePortfolioResource(Resource):
     @jwt_required()
     def post(self, coin_id):
-        current_user = get_jwt_identity()
-        profile = Profile.query.filter_by(username=current_user).first()
-
-        portfolio = Portfolio(coin_id=coin_id, profile=profile)
+        portfolio = Portfolio(coin_id=coin_id, profile=current_user)
         portfolio.save()
 
         return {"message": f"Added {coin_id} to portfolio"}, HTTPStatus.CREATED
 
     @jwt_required()
     def delete(self, coin_id):
-        current_user = get_jwt_identity()
-        profile = Profile.query.filter_by(username=current_user).first()
-        portfolio = Portfolio.query.filter_by(profile=profile).filter_by(coin_id=coin_id).delete()
+        portfolio = Portfolio.query.filter_by(profile=current_user).filter_by(coin_id=coin_id).delete()
         db.session.commit()
 
         return {"message": f"Removed {coin_id} from portfolio"}, HTTPStatus.NO_CONTENT
