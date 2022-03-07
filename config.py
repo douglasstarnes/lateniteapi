530f5cdc4116c8295d9eef6ada878c22da46c2a4
Douglas Starnes <douglas.a.starnes@gmail.com>
2022-03-06 21:04:32 -0600

    added secrets in .env for prod config file

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 class DevConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
@@ -11,6 +16,6 @@ class DevPsqlConfig:
     JWT_SECRET_KEY = "yekterces"
 
 class ProdConfig:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:PA55word@localhost/late_nite_api"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRES_CON_STR")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "yekterces"
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app import create_app
 
-from config import DevPsqlConfig
+from config import ProdConfig
 
 from app.models.profile import Profile
 from app.models.watchlist import Watchlist
 from app.models.coin import Coin
 
-app = create_app(DevPsqlConfig)
+app = create_app(ProdConfig)
