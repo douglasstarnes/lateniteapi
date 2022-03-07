e6bfd341329d67c8493c527c9d83a2738ddf00fe
Douglas Starnes <douglas.a.starnes@gmail.com>
2022-03-06 20:49:48 -0600

    config for postgres

--- config.py
+++ config.py
@@ -4,7 +4,13 @@ class DevConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "yekterces"
 
+class DevPsqlConfig:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:PA55word@localhost/late_nite_api"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "yekterces"
+
 class ProdConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:PA55word@localhost/late_nite_api"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "yekterces"
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app import create_app
 
-from config import DevConfig
+from config import DevPsqlConfig
 
 from app.models.profile import Profile
 from app.models.watchlist import Watchlist
 from app.models.coin import Coin
 
-app = create_app(DevConfig)
+app = create_app(DevPsqlConfig)
